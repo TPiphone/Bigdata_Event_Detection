dd10f4f8134f649005d090bac803e76c9c15e7fd
TPiphone <23555289@sun.ac.za>
2024-08-06 14:35:47 +0200

    plotting perfect

--- bigdata_detection/modules/plot.py
+++ bigdata_detection/modules/plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sys
-sys.path.append('../../definitions')
+sys.path.append('../definitions')
 import definitions_plotting as defplot
 from datetime import datetime
 
